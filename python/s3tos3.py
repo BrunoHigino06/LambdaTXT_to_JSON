8c94b0d019b493ab18cfc14f23b1a017d01d52c2
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-03-05 20:28:58 -0300

    Update s3tos3.py

--- python/s3tos3.py
+++ python/s3tos3.py
@@ -1,7 +1,10 @@
 import json
+import boto3
+
+s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
-    # TODO implement
+    
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
