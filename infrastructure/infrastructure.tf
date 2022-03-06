22b43ed53e835a22ef0921f117715fbd61973587
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-03-05 22:07:14 -0300

    Update infrastructure.tf

--- infrastructure/infrastructure.tf
+++ infrastructure/infrastructure.tf
@@ -1,3 +1,5 @@
+# Lambda Function
+
 resource "aws_lambda_function" "S3ToS3" {
   filename      = "./python/s3tos3.zip"
   function_name = "S3ToS3"
@@ -14,6 +16,8 @@ resource "aws_lambda_function" "S3ToS3" {
   }
 }
 
+
+# S3 source and destination
 
 resource "aws_s3_bucket" "SourceBucket" {
   name = "SourceBucket${timestamp()}"
@@ -41,4 +45,21 @@ resource "aws_s3_bucket" "DestBucket" {
 resource "aws_s3_bucket_acl" "DestBucketACL" {
   bucket = aws_s3_bucket.DestBucket.id
   acl    = "private"
+}
+
+# Adding S3 bucket as trigger to my lambda and giving the permissions
+resource "aws_s3_bucket_notification" "aws-lambda-trigger" {
+  bucket = aws_s3_bucket.SourceBucket.bucket
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.S3ToS3.arn
+    events              = ["s3:ObjectCreated:*"]
+
+  }
+}
+resource "aws_lambda_permission" "LambdaS3Acess" {
+  statement_id  = "LambdaS3Acess"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.S3ToS3.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = "arn:aws:s3:::${aws_s3_bucket.SourceBucket.id}"
 }
